533fa61086ca87a7a83ffb65bddca5fece4579dc
Clament <sclament@gmail.com>
2025-10-11 23:52:14 +0530

    changed to api common

--- app/route_controller/service_route.py
+++ app/route_controller/service_route.py
@@ -6,19 +6,19 @@ from app.admin_controller.service_provided import (
 
 service_bp = Blueprint('service_bp', __name__)
 
-service_bp.route('/api/create-service', methods=['POST'])(service_create)
-service_bp.route('/api/service-login', methods=['POST'])(service_login)
-service_bp.route('/api/service-logout', methods=['GET'])(service_logout)
-# service_bp.route('/api/service-change-password', methods=['PUT'])(service_change_password)
-service_bp.route('/api/service-forgot-password', methods=['POST'])(service_forgot_password)
+service_bp.route('/create-service', methods=['POST'])(service_create)
+service_bp.route('/service-login', methods=['POST'])(service_login)
+service_bp.route('/service-logout', methods=['GET'])(service_logout)
+# service_bp.route('/service-change-password', methods=['PUT'])(service_change_password)
+service_bp.route('/service-forgot-password', methods=['POST'])(service_forgot_password)
 
-service_bp.route('/api/service-reset-password', methods=['POST'])(service_reset_password)
+service_bp.route('/service-reset-password', methods=['POST'])(service_reset_password)
 
-service_bp.route("/api/generate", methods=["POST"])(generate_coupon)
-service_bp.route('/api/coupon-use', methods=['POST'])(use_coupon)
+service_bp.route("/generate", methods=["POST"])(generate_coupon)
+service_bp.route('/coupon-use', methods=['POST'])(use_coupon)
 
 
-service_bp.route('/api/create-user-service', methods=['POST'])(create_user_service)
-service_bp.route('/api/create-user-course', methods=['POST'])(create_user_course)
-service_bp.route('/api/user-services', methods=['GET'])(get_user_services)
-service_bp.route('/api/user-courses', methods=['GET'])(get_user_courses)
+service_bp.route('/create-user-service', methods=['POST'])(create_user_service)
+service_bp.route('/create-user-course', methods=['POST'])(create_user_course)
+service_bp.route('/user-services', methods=['GET'])(get_user_services)
+service_bp.route('/user-courses', methods=['GET'])(get_user_courses)
