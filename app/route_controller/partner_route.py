c3e349ca22391c69d0a5dd7a316bb7cf8d734135
Subash Chan <subashcb7799@gmail.com>
2025-10-11 15:37:12 +0530

    /api added in routes

--- app/route_controller/partner_route.py
+++ app/route_controller/partner_route.py
@@ -8,16 +8,16 @@ from app.auth_controller.partner_controller import (
 
 partner_bp = Blueprint("partner_bp", __name__)
 
-partner_bp.route("/approve", methods=["POST"])(approve_partner_request)
-partner_bp.route("/decline", methods=["POST"])(decline_partner_request)
-partner_bp.route("/confirm", methods=["POST"])(handle_partner_request)
+partner_bp.route("/api/approve", methods=["POST"])(approve_partner_request)
+partner_bp.route("/api/decline", methods=["POST"])(decline_partner_request)
+partner_bp.route("/api/confirm", methods=["POST"])(handle_partner_request)
 # partner_bp.route("/refer-user", methods=["POST"])(refer_user)
 
 # partner_bp.route("/make", methods=["POST"])(make_partner)
-partner_bp.route("/partner-dashboard", methods=["GET"])(partner_dashboard)
-# partner_bp.route("/commission", methods=["POST"])(give_monthly_commission)
+partner_bp.route("/api/partner-dashboard", methods=["GET"])(partner_dashboard)
+# partner_bp.route("/api/commission", methods=["POST"])(give_monthly_commission)
 
 
-partner_bp.route("/wallet-withdraw-request", methods=["POST"])(request_partner_wallet_withdrawal)
-partner_bp.route("/wallet-withdraw-approve", methods=["POST"])(approve_partner_wallet_withdrawal)
-partner_bp.route("/wallet-withdraw-decline", methods=["POST"])(decline_partner_wallet_withdrawal)
+partner_bp.route("/api/wallet-withdraw-request", methods=["POST"])(request_partner_wallet_withdrawal)
+partner_bp.route("/api/wallet-withdraw-approve", methods=["POST"])(approve_partner_wallet_withdrawal)
+partner_bp.route("/api/wallet-withdraw-decline", methods=["POST"])(decline_partner_wallet_withdrawal)
